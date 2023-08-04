8c03628a66728635dce65f5a1492219fc5a76ec5
N4rdelli <nardellianelize20@gmail.com>
2023-08-04 16:44:22 -0300

    Projeto Finalizado.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+ /* Arrow Function, age da mesma maneira que um Function, mas é mais prático */
+var toggleMode = () => {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+}
